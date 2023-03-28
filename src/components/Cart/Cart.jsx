b197f7b1d118a4c4fd7b01567a9b5b3555a65ac4
BidhanCR <bidhanroy300805@gmail.com>
2023-03-28 20:18:35 +0600

    show data from cart

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 import './Cart.css';
 const Cart = ({cart}) => {
     // const {cart} = props;
-    console.log(cart);
+    // console.log(cart);
     let totalPrice = 0;
     let totalShipping = 0;
+    let quantity = 0;
     for(const product of cart){
-        totalPrice = totalPrice + product.price;
+        product.quantity = product.quantity || 1;
+        totalPrice = totalPrice + product.price * product.quantity;
         totalShipping = totalShipping + product.shipping;
+        quantity = quantity + product.quantity;
     }
 
     const tax = totalPrice*7/100;
@@ -15,7 +18,7 @@ const Cart = ({cart}) => {
     return (
         <div className='cart'>
             <h5>Order summary</h5>
-            <p>Selected items: {cart.length}</p>
+            <p>Selected items: {quantity}</p>
             <p>Total Price: ${totalPrice}</p>
             <p>Total Shipping Charge: ${totalShipping}</p>
             <p>Tax: ${tax.toFixed(2)}</p>
